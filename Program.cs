70bacbcac63c1c22de3e47a51b07400e545cdcb0
Venugopal K <venu.shastri@gmail.com>
2021-07-10 11:50:28 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+
+
+class Program{
+
+ static double Calculate(int x,double y){ return x+y ;}
+ 
+ void Update(int newNumber){ System.Console.WriteLine(newNumber); }
+ 
+ void Delete(string key){}
+
+staic void FunctionsAsAnArguments(Action obj,Action<string> obj2, Func<int,int> obj3, Func<string,string,bool> obj4){
+
+//Invoke Delegate objects
+
+
+}
+
+static void Main(){
+
+//Call FunctionAsAnArguments 
+
+}
+
+}
